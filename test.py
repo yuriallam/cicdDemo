0c543acd8b4eceaa5bf3b3aea6b72088ada45cf3
Yuri_Allam <yuriallam7@gmail.com>
2023-09-22 12:53:23 +0300

    testing

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from main import add
 class TestMathOperations(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(1, 2), 2)
+        self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(0, 0), 0)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
